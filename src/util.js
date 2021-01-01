5fd631192c1bfb4a3647d13ca669d5f7b4b10e1b
its4zahoor <its4zahoor@gmail.com>
2021-01-02 02:11:28 +0500

    fixes bug in time formatting

--- src/util.js
+++ src/util.js
@@ -1,8 +1,12 @@
+const H_mSec = 60 * 60 * 1000;
+const M_mSec = 60 * 1000;
+const S_mSec = 1000;
+
 export const formatTime = (mSec) => {
   const ms = mSec % 1000;
-  const s = Math.floor(mSec / 1000);
-  const m = Math.floor(s / 60);
-  const h = Math.floor(m / 60);
+  const s = Math.floor((mSec / S_mSec) % 60);
+  const m = Math.floor((mSec / M_mSec) % 60);
+  const h = Math.floor(mSec / H_mSec);
 
   const padMs = ms.toString().padStart(3, "0");
   const padS = s.toString().padStart(2, "0");
